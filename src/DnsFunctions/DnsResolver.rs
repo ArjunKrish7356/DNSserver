76e2e468963cd0d95cce5e015f7fa8a8f4d757a4
ArjunKrish7356 <arjunkrishna7356@gmail.com>
2024-12-15 10:48:39 +0530

    syncronous single use Dns Resolver implemented

--- src/DnsFunctions/DnsResolver.rs
+++ src/DnsFunctions/DnsResolver.rs
@@ -1,134 +1,127 @@
 mod Dnspacket;
 use std::{net::{IpAddr, Ipv4Addr, UdpSocket,SocketAddr}};
-use std::io::Error as E;
+use std::fmt;
+use std::error::Error;
+
+
+
 
 use Dnspacket::DnsRecord;
 
-fn test_main() -> Result<(), Box<dyn std::error::Error>>{
-    let mut packet = Dnspacket::DnsPacket::new();
-
-    //create a socket and recieve the packet
-    let in_socket = UdpSocket::bind("127.0.0.1:2053").unwrap();
-    let mut root_query_buffer = Dnspacket::BytePacketBuffer::new();
-    let (_, src) = in_socket.recv_from(&mut root_query_buffer.buf).unwrap();
-    let mut packet1 = Dnspacket::DnsPacket::from_buffer(&mut root_query_buffer).unwrap();
-
-    println!("stage2");
-    let root_server = (
-        IpAddr::V4("192.203.230.10".parse::<Ipv4Addr>().unwrap()),
-        53,
-    );
-
-    let out_socket = UdpSocket::bind("0.0.0.0:0").unwrap(); // Use any available local address and port
-    let no = out_socket.send_to(
-        &root_query_buffer.buf[0..root_query_buffer.pos],
-        root_server,
-    ).unwrap();
-    //get the data from the root server
-    println!("stage3");
-    let mut root_answer_buffer = Dnspacket::BytePacketBuffer::new();
-    out_socket.recv_from(&mut root_answer_buffer.buf).unwrap();
-    let packet2 = Dnspacket::DnsPacket::from_buffer(&mut root_answer_buffer).unwrap();
-    let ns = random_ns(&packet2.resources).unwrap();
-    println!("{:#?}",ns);
-    Ok(())
+#[derive(Debug)]
+pub enum DnsResolverError {
+    NetworkError(std::io::Error),
+    ParseError(String),
+    NoQuestionFound,
+    NoNameserverFound,
+    ResolutionFailed,
 }
 
-
-//main fn will listen in the socket out of pc
-//when a query comes it gives data to recursive resolver
-    //inside a while loop it will call the name server untill the query matches
-//rescursive resolver will give back the finished dns packet
-
-
-
-
-fn main() -> Result<(), Box<dyn std::error::Error>>{
-     let in_socket = UdpSocket::bind("127.0.0.1:2053").unwrap();
-     let mut root_query_buffer = Dnspacket::BytePacketBuffer::new();
-   
-
-
-        let (_,src) = in_socket.recv_from(&mut root_query_buffer.buf).unwrap();
-        let buffer = recursive_resolver(src,root_query_buffer.clone()).unwrap();
-        in_socket.send_to(&buffer.buf[0..buffer.pos],src);
-
-    Ok(())
+impl fmt::Display for DnsResolverError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            DnsResolverError::NetworkError(err) => 
+                write!(f, "Network error: {}", err),
+            DnsResolverError::ParseError(details) => 
+                write!(f, "Parsing error: {}", details),
+            DnsResolverError::NoQuestionFound => 
+                write!(f, "No DNS question found in the packet"),
+            DnsResolverError::NoNameserverFound => 
+                write!(f, "Unable to find a valid nameserver"),
+            DnsResolverError::ResolutionFailed => 
+                write!(f, "DNS resolution failed"),
+        }
+    }
 }
 
-fn recursive_resolver(src: SocketAddr, buffer: Dnspacket::BytePacketBuffer) -> Option<Dnspacket::BytePacketBuffer> {
-    let mut buffer = buffer.clone();
-    let mut buffer2 = buffer.clone();
-    let packet = Dnspacket::DnsPacket::from_buffer(&mut buffer).unwrap();
-    println!("{:#?}", packet);
+impl Error for DnsResolverError {}
 
-    let domain = if let Some(Dnspacket::DnsQuestion { name, .. }) = packet.questions.get(0) {
-        name
-    } else {
-        eprintln!("No valid DNS question found at index 0.");
-        return None;
-    };
+impl From<std::io::Error> for DnsResolverError {
+    fn from(err: std::io::Error) -> Self {
+        DnsResolverError::NetworkError(err)
+    }
+}
+
+fn recursive_resolver(
+    buffer: Dnspacket::BytePacketBuffer,
+) -> Result<Dnspacket::BytePacketBuffer,DnsResolverError> {
+    let mut buffer = buffer;
+    let mut packet = Dnspacket::DnsPacket::from_buffer(&mut buffer)
+        .map_err(|e|DnsResolverError::ParseError(e.to_string()))?;
+
+    let domain = packet.questions.get(0)
+        .map(|q| &q.name)
+        .ok_or(DnsResolverError::NoQuestionFound)?;
 
     let mut ip = String::from("192.203.230.10");
-    let out_socket = UdpSocket::bind("0.0.0.0:0").unwrap(); // Persistent socket    
+    let out_socket = UdpSocket::bind("0.0.0.0:0")?;
 
     for _ in 0..13 {
-        let Some(ns_response) = fetch_ns(&buffer, &ip, &out_socket) else {
-            eprintln!("Failed to fetch NS response.");
-            return None;
-        };
-        println!("{:#?}", ns_response);
-        if ns_response.0 == *domain {
+        let (ns_domain, ns_ip ) = fetch_ns(&buffer, &ip, &out_socket)
+            .ok_or(DnsResolverError::NoNameserverFound)?;
+
+        if ns_domain == *domain{
             break;
         }
-        ip = ns_response.1.to_string();
+        ip = ns_ip.to_string();
     }
 
-    let mut packet = Dnspacket::DnsPacket::from_buffer(&mut buffer2).unwrap();
-    let answer_record = DnsRecord::A { domain: domain.clone(), addr: ip.parse::<Ipv4Addr>().unwrap(), ttl: 1000 };
+    let answer_record = DnsRecord::A {
+        domain: domain.clone(),
+        addr: ip.parse::<Ipv4Addr>().unwrap(),
+        ttl: 1000,
+    };
     packet.answers.push(answer_record);
-    println!("{:#?} --- {}", packet,src);
     let mut response_buffer = Dnspacket::BytePacketBuffer::new();
     packet.write(&mut response_buffer).unwrap();
-    Some(response_buffer)
-
+    Ok(response_buffer)
 }
 
-fn fetch_ns<'a>(
-    buffer: &'a Dnspacket::BytePacketBuffer,
+fn fetch_ns(
+    buffer: &Dnspacket::BytePacketBuffer,
     ip_addr: &str,
     out_socket: &UdpSocket,
 ) -> Option<(String, Ipv4Addr)> {
-    let root_server = (
-        IpAddr::V4(ip_addr.parse::<Ipv4Addr>().unwrap()),
-        53,
-    );
+    let root_server = (IpAddr::V4(ip_addr.parse::<Ipv4Addr>().unwrap()), 53);
 
-    out_socket.send_to(
-        &buffer.buf[0..buffer.pos], // Later adjust to send only relevant part
-        root_server,
-    ).unwrap();
+    out_socket
+        .send_to(
+            &buffer.buf[0..buffer.pos], // Later adjust to send only relevant part
+            root_server,
+        )
+        .unwrap();
 
     let mut root_answer_buffer = Dnspacket::BytePacketBuffer::new();
     let (size, _) = out_socket.recv_from(&mut root_answer_buffer.buf).unwrap();
 
     let packet2 = Dnspacket::DnsPacket::from_buffer(&mut root_answer_buffer).unwrap();
-    if packet2.header.answers !=0{
-        return random_ns( &packet2.answers);
+    if packet2.header.answers != 0 {
+        return random_ns(&packet2.answers);
     }
     let ns = random_ns(&packet2.resources)?;
     Some(ns)
 }
 
-
 fn random_ns<'a>(list: &'a Vec<Dnspacket::DnsRecord>) -> Option<(String, Ipv4Addr)> {
     for record in list {
-        match record{
-            Dnspacket::DnsRecord::A {domain,addr,ttl} => {
-                return Some((domain.clone(),*addr))
-            }
-            _ => ()
+        match record {
+            Dnspacket::DnsRecord::A { domain, addr, ttl } => return Some((domain.clone(), *addr)),
+            _ => (),
         }
     }
     None
+}
+
+fn main() -> Result<(), DnsResolverError> {
+    let in_socket = UdpSocket::bind("127.0.0.1:2054")?;
+
+    let mut root_query_buffer = Dnspacket::BytePacketBuffer::new();
+
+    let (_, src) = in_socket.recv_from(&mut root_query_buffer.buf)?;
+    
+    let mut buffer = recursive_resolver(root_query_buffer.clone())?;
+
+    in_socket.send_to(&buffer.buf[0..buffer.pos], src)?;
+
+    Ok(())
 }
--- src/DnsFunctions/Dnspacket.rs
+++ src/DnsFunctions/Dnspacket.rs
@@ -3,7 +3,7 @@ use std::io::{Error as E, ErrorKind, Read};
 use std::net::Ipv6Addr;
 use std::net::{Ipv4Addr, UdpSocket};
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 pub struct BytePacketBuffer {
     pub buf: [u8; 4096],
     pub pos: usize,
@@ -635,7 +635,7 @@ impl DnsPacket {
 
     pub fn from_buffer(buffer: &mut BytePacketBuffer) -> Result<DnsPacket, E> {
         let mut result = DnsPacket::new();
-        result.header.read(buffer);
+        result.header.read(buffer)?;
 
         for _ in 0..result.header.questions {
             let mut question = DnsQuestion::new("".to_string(), QueryType::UNKNOWN(0));
@@ -683,86 +683,5 @@ impl DnsPacket {
         }
 
         Ok(())
-    }
-}
-
-fn lookup(qname: &str, qtype: QueryType) -> Result<DnsPacket, E> {
-    // Forward queries to Google's public DNS
-    let server = ("8.8.8.8", 53);
-
-    let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
-
-    let mut packet = DnsPacket::new();
-
-    packet.header.id = 6666;
-    packet.header.questions = 1;
-    packet.header.recursion_desired = true;
-    packet
-        .questions
-        .push(DnsQuestion::new(qname.to_string(), qtype));
-
-    let mut req_buffer = BytePacketBuffer::new();
-    packet.write(&mut req_buffer)?;
-    socket.send_to(&req_buffer.buf[0..req_buffer.pos], server)?;
-
-    let mut res_buffer = BytePacketBuffer::new();
-    socket.recv_from(&mut res_buffer.buf)?;
-
-    DnsPacket::from_buffer(&mut res_buffer)
-}
-
-pub fn handle_query(socket: &UdpSocket) -> Result<(), E> {
-    let mut buf = BytePacketBuffer::new();
-    let (_, src) = socket.recv_from(&mut buf.buf)?;
-
-    // create a new dns packet and set the values of it from the query_packet
-    let mut query_packet = DnsPacket::from_buffer(&mut buf)?;
-    let mut packet = DnsPacket::new();
-    packet.header.id = query_packet.header.id;
-    packet.header.recursion_desired = true;
-    packet.header.questions = 1;
-    packet.header.recursion_available = true;
-    packet.header.response = true;
-
-    if let Some(question) = query_packet.questions.pop() {
-        if let Ok(result) = lookup(&question.name, question.qtype) {
-            packet.questions.push(question);
-            packet.header.rescode = result.header.rescode;
-
-            for rec in result.answers {
-                println!("Answer: {:?}", rec);
-                packet.answers.push(rec);
-            }
-            for rec in result.authorities {
-                println!("Authority: {:?}", rec);
-                packet.authorities.push(rec);
-            }
-            for rec in result.resources {
-                println!("Resource: {:?}", rec);
-                packet.resources.push(rec);
-            }
-        } else {
-            packet.header.rescode = ResultCode::SERVFAIL;
-        }
-    } else {
-        packet.header.rescode = ResultCode::FORMERR;
-    }
-
-    let mut res_buf = BytePacketBuffer::new();
-    packet.write(&mut res_buf);
-    let buf_len = res_buf.pos();
-    let data = res_buf.get_range(0, buf_len)?;
-
-    socket.send_to(&data, src);
-    Ok(())
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let socket = UdpSocket::bind("127.0.0.1:2053")?;
-    loop {
-        match handle_query(&socket) {
-            Ok(_) => {}
-            Err(e) => eprintln!("An error occurred: {}", e),
-        }
     }
 }
